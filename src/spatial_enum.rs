afb45e4129df7c9998bf4d2ed61b6de5255890b7
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-12-26 04:51:55 +0100

    :sparkles: Reworked enums

--- src/spatial_enum.rs
+++ src/spatial_enum.rs
@@ -1,17 +1,79 @@
 use proc_macro::TokenStream;
-use syn::parse_macro_input;
-use syn::ItemEnum;
+use syn::{parse_macro_input, ItemEnum};
+
+use crate::ast::EnumAST;
+
+impl<'a> Into<TokenStream> for EnumAST<'a> {
+    fn into(self) -> TokenStream {
+        let name = self.name;
+
+        let from_u32 = {
+            let variants = self
+                .variants
+                .iter()
+                .map(|variant| {
+                    let value = variant.value;
+                    let ident = &variant.name;
+                    quote! { #value => #name :: #ident}
+                })
+                .collect::<Vec<_>>();
+            quote! {
+                impl From<u32> for #name {
+                    fn from(data: u32) -> Self {
+                        match data {
+                            #(#variants,)*
+                            _ => panic!("Invalid data")
+                        }
+                    }
+                }
+            }
+        };
+
+        let into_u32 = {
+            let variants = self
+                .variants
+                .iter()
+                .map(|variant| {
+                    let value = variant.value;
+                    let ident = &variant.name;
+                    quote! { #name :: #ident => #value}
+                })
+                .collect::<Vec<_>>();
+            quote! {
+                impl Into<u32> for #name {
+                    fn into(self) -> u32 {
+                        match self {
+                            #(#variants,)*
+                        }
+                    }
+                }
+
+                impl Into<u32> for &#name {
+                    fn into(self) -> u32 {
+                        match *self {
+                            #(#variants,)*
+                        }
+                    }
+                }
+            }
+        };
+
+        let result = quote! {
+            #[automatically_derived]
+            #[allow(unused_qualifications)]
+            #from_u32
+
+            #[automatically_derived]
+            #[allow(unused_qualifications)]
+            #into_u32
+        };
+        result.into()
+    }
+}
 
 pub fn generate_enum(item: TokenStream) -> TokenStream {
     let input = parse_macro_input!(item as ItemEnum);
 
-    let enum_name = &input.ident;
-    let variants = &input.variants;
-
-    let result = quote! {
-        pub enum #enum_name {
-            #variants
-        }
-    };
-    result.into()
+    let ast = EnumAST::from(&input);
+    ast.into()
 }
